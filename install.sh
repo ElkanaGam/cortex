ff20cc682e108837da838f8d30a1aae1d722bb25
Elkana Gamliel <40407541+ElkanaGam@users.noreply.github.com>
2020-05-31 02:00:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,15 @@
-# see running dockers
-sudo docker ps
-# activate rabbitmq
-sudo docker run -d -p 5672:5672 rabbitmq
-# kill processes on specific port
-sudo kill -9 $(sudo lsof -t -i:<port>)
-# run postgres docker
-sudo docker run -d -p 5432:5432 -e POSTRGES_USER=postgres -e POSTGRES_PASSWORD=password postgres
-# run postgres shell
-psql -h 127.0.0.1 -p 5432 -U postgres   #password =*password*
-# client
-python -m client upload-sample -h 127.0.0.1 -p 8000 ./sample.mind
-# server
-python -m server run-server -h 127.0.01 -p 8000 rabbitmq://127.0.0.1:5672/
-# parsers
-python -m parsers run-all
-# saver
-python -m saver run-saver  postgresql://127.0.0.1:5432
+#!/bin/bash
 
-#activeate venv
-C:\temp\shared\.env\Scripts\activate.bat
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-#move to directory
-cd ../../temp/shared
 
-#run api server
-python -m api run-api-server
+function main {
+    python -m virtualenv .env --prompt "[cortex] "
+    find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
+    pip install -U pip
+    pip install -r requirements.txt
+}
+
+
+main "$@"
