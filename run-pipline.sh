4f364c3d1012b57d6ed2ad63a7eab726fa848f68
Elkana Gamliel <40407541+ElkanaGam@users.noreply.github.com>
2020-05-31 19:52:28 +0300

    Update run-pipline.sh

--- run-pipline.sh
+++ run-pipline.sh
@@ -5,7 +5,7 @@ sudo docker run -d -p 5672:5672 rabbitmq>/dev/null
 # run postgres docker
 sudo docker run -d -p 5432:5432 -e POSTRGES_USER=postgres -e POSTGRES_PASSWORD=elkana1 postgres>/dev/null
 #cretae database 
-psql -U postgres -c "CREATE DATABASE mydb;" 
+psql -U postgres -c "CREATE DATABASE db;" 
 #connect to db
 python -m cotrex.saver run-saver  postgresql://127.0.0.1:5432&>/dev/null&
 # start parse
